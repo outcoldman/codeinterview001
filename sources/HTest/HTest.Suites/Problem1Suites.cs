c8c32f02c68e17763b73a6c683447b6df2907b44
Denis Gladkikh <outcoldman@gmail.com>
2013-09-06 22:23:51 -0700

    Add Problem1 solution

---
+++ sources/HTest/HTest.Suites/Problem1Suites.cs
@@ -0,0 +1,115 @@
+﻿namespace HTest.Suites
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    public class Problem1Suites
+    {
+        [Test]
+        public void FindSubsequence_NullArray_ThrowsException()
+        {
+            // Arrange
+            int[] sequence = null;
+
+            // Act
+            TestDelegate act = () => Problem1.FindSubsequence(sequence);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(act, "FindSubsequence should throw exception when sequence is null.");
+        }
+
+        [Test]
+        public void FindSubsequence_EmptyArray_ThrowsException()
+        {
+            // Arrange
+            int[] sequence = new int[0];
+
+            // Act
+            TestDelegate act = () => Problem1.FindSubsequence(sequence);
+
+            // Assert
+            Assert.Throws<ArgumentException>(act, "FindSubsequence should throw exception when sequence is empty.");
+        }
+
+        [Test]
+        public void FindSubsequence_OneElement_ReturnsArrayWithOneElement()
+        {
+            // Arrange
+            int[] sequence = new int[] { 1 };
+
+            // Act
+            int[] result = Problem1.FindSubsequence(sequence);
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(new [] { 1 }, result) , "Result should be [ 1 ], but was [ {0}]", String.Join(", ", result.Select(x => x.ToString(CultureInfo.InvariantCulture))));
+        }
+
+        [Test]
+        public void FindSubsequence_OneConsecutiveSequence_ReturnsWholeArray()
+        {
+            // Arrange
+            int[] sequence = new int[] { 1, 2, 3, 4 };
+
+            // Act
+            int[] result = Problem1.FindSubsequence(sequence);
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { 1, 2, 3, 4 }, result), "Result should be [ 1, 2, 3, 4 ], but was [ {0}]", String.Join(", ", result.Select(x => x.ToString(CultureInfo.InvariantCulture))));
+        }
+
+        [Test]
+        public void FindSubsequence_TwoConsecutiveSequencesWithTheSameLength_ReturnsFirstSubsequence()
+        {
+            // Arrange
+            int[] sequence = new int[] { 1, 2, 3, 4, 2, 3, 4, 5 };
+
+            // Act
+            int[] result = Problem1.FindSubsequence(sequence);
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { 1, 2, 3, 4 }, result), "Result should be [ 1, 2, 3, 4 ], but was [ {0}]", String.Join(", ", result.Select(x => x.ToString(CultureInfo.InvariantCulture))));
+        }
+
+        [Test]
+        public void FindSubsequence_TwoConsecutiveSequencesWhereSecondHasMoreElements_ReturnsSecondSubsequence()
+        {
+            // Arrange
+            int[] sequence = new int[] { 1, 2, 3, 4, 2, 3, 4, 5, 6 };
+
+            // Act
+            int[] result = Problem1.FindSubsequence(sequence);
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { 2, 3, 4, 5, 6 }, result), "Result should be [ 2, 3, 4, 5, 6 ], but was [ {0}]", String.Join(", ", result.Select(x => x.ToString(CultureInfo.InvariantCulture))));
+        }
+
+        [Test]
+        public void FindSubsequence_ConsecutiveSequenceWithMoreElementsInCenter_ReturnsSubsequence()
+        {
+            // Arrange
+            int[] sequence = new int[] { 1, 2, 3, 4, 2, 3, 4, 5, 6, 1, 2, 3, 4 };
+
+            // Act
+            int[] result = Problem1.FindSubsequence(sequence);
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { 2, 3, 4, 5, 6 }, result), "Result should be [ 2, 3, 4, 5, 6 ], but was [ {0}]", String.Join(", ", result.Select(x => x.ToString(CultureInfo.InvariantCulture))));
+        }
+
+        [Test]
+        public void FindSubsequence_ConsecutiveSequenceWithNegativeElements_ReturnsSubsequence()
+        {
+            // Arrange
+            int[] sequence = new int[] { 1, 2, 3, 4, -2, -1, 0, 1, 2, 1, 2, 3, 4 };
+
+            // Act
+            int[] result = Problem1.FindSubsequence(sequence);
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { -2, -1, 0, 1, 2 }, result), "Result should be [ -2, -1, 0, 1, 2 ], but was [ {0}]", String.Join(", ", result.Select(x => x.ToString(CultureInfo.InvariantCulture))));
+        }
+    }
+}
---
+++ sources/HTest/HTest/Problem1.cs
@@ -0,0 +1,71 @@
+﻿namespace HTest
+{
+    using System;
+
+    public class Problem1
+    {
+        /// <summary>
+        /// Find the longest consecutive sub-sequence of increasing numbers.
+        /// </summary>
+        /// <param name="sequence">Array of integers.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="sequence"/> is null.</exception>
+        /// <exception cref="ArgumentException">If <paramref name="sequence"/> is empty.</exception>
+        /// <returns>Array of sub-sequence.</returns>
+        public static int[] FindSubsequence(int[] sequence)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentNullException("sequence");
+            }
+
+            if (sequence.Length == 0)
+            {
+                throw new ArgumentException(Strings.ErrMsg_SequenceShouldNotBeEmpty, "sequence");
+            }
+
+            int maxSequenceIndex = 0;
+            int maxSequenceLength = 1;
+
+            int sequenceIndex = 0;
+            int sequenceLength = 1;
+
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                if (sequence[i] - sequence[i - 1] == 1)
+                {
+                    if (sequenceIndex + sequenceLength == i)
+                    {
+                        sequenceLength++;
+                    }
+                    else
+                    {
+                        sequenceIndex = i - 1;
+                        sequenceLength = 2;
+                    }
+                }
+                else
+                {
+                    if (sequenceLength > maxSequenceLength)
+                    {
+                        maxSequenceIndex = sequenceIndex;
+                        maxSequenceLength = sequenceLength;
+                    }
+                }
+            }
+
+            if (sequenceLength > maxSequenceLength)
+            {
+                maxSequenceIndex = sequenceIndex;
+                maxSequenceLength = sequenceLength;
+            }
+
+            int[] result = new int[maxSequenceLength];
+            for (int i = 0; i < maxSequenceLength; i++)
+            {
+                result[i] = sequence[maxSequenceIndex + i];
+            }
+
+            return result;
+        }
+    }
+}
